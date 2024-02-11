c970b73bd511a6b1cd86b99bd1d8ca6670223f01
Pratik01-git <Pratik01-git>
2024-02-11 17:44:05 +0530

    Minor responsive changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,18 +14,18 @@ function Header({loadData}) {
   }
 
   return (
-    <header className='bg-white flex justify-between p-2'>
-      <div>
-        <h1 className='font-mono font-bold text-4xl pl-2 text-sky-500 cursor-default'>ContactBook</h1>
+    <header className='bg-white flex flex-col sm:flex-row justify-between p-2'>
+      <div className='text-center'>
+        <h1 className='font-mono font-bold text-2xl sm:text-4xl pl-2 text-sky-500 cursor-default'>ContactBook</h1>
       </div>
 
-      <form onSubmit={(e)=>handleSubmit(e)} className='flex justify-end items-center'>
-        <input type="text" name="page" placeholder='Page no.' value={page} onChange={(e)=>setPage(e.target.value)} className='bg-sky-300/35 mr-1 focus:outline-none p-1'/>
-        <input type="text" name="contacts" placeholder='Contacts per page' value={contacts} onChange={(e)=>setContacts(e.target.value)} className='bg-sky-300/35 mr-1 focus:outline-none p-1'/>
-        <button type="submit" className='mx-1 bg-cyan-400 px-2 py-1 rounded-md text-white'>Go</button>
+      <form onSubmit={(e)=>handleSubmit(e)} className='grid grid-cols-8'>
+        <input type="text" name="page" placeholder='Page no.' value={page} onChange={(e)=>setPage(e.target.value)} className='bg-sky-300/35 mr-1 focus:outline-none p-1 col-span-3'/>
+        <input type="text" name="contacts" placeholder='Contacts per page' value={contacts} onChange={(e)=>setContacts(e.target.value)} className='bg-sky-300/35 mr-1 focus:outline-none p-1 col-span-3'/>
+        <button type="submit" className='mx-1 bg-cyan-400 px-2 py-1 rounded-md text-white col-span-2'>Go</button>
       </form>
-
     </header>
+
   )
 }
 
